d25293aaa265009bc5bfb2c662c90c25167f5c5c
Keith P. Jolley <keith.jolley@light.co>
2021-09-05 09:51:54 -0700

    service

---
+++ scrape.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd '/home/kjolley/html/altgnat/sdgeweatherscrape'
+
+. 'venv/bin/activate'
+
+./scrape.py
+
--- scrapetimer/service.start.sh
+++ scrapetimer/service.start.sh
@@ -32,9 +32,9 @@ then
 fi
 
 # Test
-systemctl --user enable sdgescraper.service
-systemctl --user enable sdgescraper.service
+#systemctl --user enable sdgescraper.service
+#systemctl --user enable sdgescraper.service
 
 # Install
-#systemctl --user enable sdgescraper.timer
-#systemctl --user enable sdgescraper.timer
+systemctl --user enable sdgescraper.timer
+systemctl --user enable sdgescraper.timer
