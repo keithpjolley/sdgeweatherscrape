6f724b69e31a99a721a47119b0e57b541a187ef0
Keith P. Jolley <keith.jolley@light.co>
2021-09-05 07:18:22 -0700

    +installer

--- scrapetimer/service.start.sh
+++ scrapetimer/service.start.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
+
+
+
+sysd="${HOME}/.config/systemd/user"
+service="${sysd}/sdgescraper.service"
+timer="${sysd}/sdgescraper.timer"
+
+if [ ! -d "${HOME}/.config/systemd/user" ]
+then 
+    mkdir "${HOME}/.config/systemd/user"
+fi
+
+if [ ! -f "${service}" -a ! -l "${service}" ]
+then
+    if [ ! -f './sdgescraper.service' ]
+    then
+        echo "ERROR: ${me}: no file './sdgescraper.service'" 1>&2
+        exit 1
+    fi
+    ln -s "$( pwd )/sdgescraper.service" "${service}"
+fi
+
+if [ ! -f "${timer}" -a ! -l "${timer}" ]
+then
+    if [ ! -f './sdgescraper.timer' ]
+    then
+        echo "ERROR: ${me}: no file './sdgescraper.timer'" 1>&2
+        exit 2
+    fi
+    ln -s "$( pwd )/sdgescraper.timer" "${timer}"
+fi
+
 # Test
 systemctl --user enable sdgescraper.service
 systemctl --user enable sdgescraper.service
