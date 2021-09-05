0454df8af691fb9e003aea539c0eaef35956089e
Keith P. Jolley <keith.jolley@light.co>
2021-09-05 07:06:11 -0700

    +service-timer

---
+++ scrapetimer/service.start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Test
+systemctl --user enable sdgescraper.service
+systemctl --user enable sdgescraper.service
+
+# Install
+#systemctl --user enable sdgescraper.timer
+#systemctl --user enable sdgescraper.timer
